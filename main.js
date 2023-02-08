357034b1f84b01c9f9efd6a345ad17a93e03574f
Linapaper <linap.r71@gmail.com>
2023-02-08 09:32:05 -0500

    inicializacion de variables a usar en la funcion, y primeros pasos de la funcion destapar

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
+//inicializacion de variable
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+
+
+// generacion de numeros aleatorios
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numeros = numeros.sort(()=>{return Math.random()-0.5})
+
+// funcion principal
+function destapar(id){
+    tarjetasDestapadas++;
+
+    if (tarjetasDestapadas == 1){
+        //mostrar primer numero
+        tarjeta1 = document.getElementById(id);
+        primerResultado = numeros[id];
+        tarjeta1.innerHTML = primerResultado;
+    }
+
+}
