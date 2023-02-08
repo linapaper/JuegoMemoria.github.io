4e1193a6969f80cc8ca01acaef06c9c4c2b3445a
Linapaper <linap.r71@gmail.com>
2023-02-08 09:45:28 -0500

    sumar movimientos a estadisticas, correcion de escritura id en html

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ let tarjeta1 = null;
 let tarjeta2 = null;
 let primerResultado = null;
 let segundoResultado = null;
+let movimientos = 0;
 
+//apuntando a documentos html 
+
+let mostrarMovimientos = document.getElementById('movimientos');
 
 // generacion de numeros aleatorios
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
@@ -30,6 +34,10 @@ function destapar(id){
 
         //desahibilitar segundo boton
         tarjeta2.disabled = true;
+
+        //incrementar movimientos
+        movimientos++;
+        mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
     }
 
 }
